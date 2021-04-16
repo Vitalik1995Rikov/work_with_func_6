5b521946d332608558755a41a38d3d64a2489d1b
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-16 19:33:29 +0300

    6.3.1

--- ex_3/script.ts
+++
@@ -1,9 +0,0 @@
-class ListNode<T> {
-    private data: T;
-    private next: ListNode<T>;
-
-    constructor(data: T, next?: ListNode<T>) {
-        this.data = data;
-        this.next = next;
-    }
-}
